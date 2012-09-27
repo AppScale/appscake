186ba40f0ef88f4ebe10870abd70eacbee9cc553
hiranya911 <hiranya911@gmail.com>
2012-09-26 17:27:48 -0700

    Styling for the 2 main forms with bootstrap and jQuery validator

---
+++ public/js/appscake_validator.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+    $('#virtual_form').validate({
+        rules: {
+            ips: {
+                required: true
+            },
+            root_password: {
+                required: true
+            },
+            user: {
+                required: true,
+                email: true
+            },
+            pass: {
+                required: true,
+                minlength: 6
+            },
+            pass2: {
+                required: true,
+                minlength: 6,
+                equalTo: pass
+            },
+            keyname: {
+                required: true
+            }
+        },
+        highlight: function(label) {
+            $(label).closest('.control-group').addClass('error');
+        },
+        success: function(label) {
+            label
+                .addClass('valid')
+                .closest('.control-group').removeClass('error');
+        }
+    });
+
+    $('#iaas_ec2_form').validate({
+        rules: {
+            user: {
+                required: true,
+                email: true
+            },
+            pass: {
+                required: true,
+                minlength: 6
+            },
+            pass2: {
+                required: true,
+                minlength: 6,
+                equalTo: pass
+            },
+            keyname: {
+                required: true
+            }
+        },
+        highlight: function(label) {
+            $(label).closest('.control-group').addClass('error');
+        },
+        success: function(label) {
+            label
+                .addClass('valid')
+                .closest('.control-group').removeClass('error');
+        }
+    });
+});
