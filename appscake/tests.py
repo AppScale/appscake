b8535df89381aa7c5bae1237788548b7c6776dfa
root <root@appscale-image0.(none)>
2013-04-26 16:43:14 -0700

    fully tested

--- appscake/tests.py
+++ appscake/tests.py
@@ -12,6 +12,13 @@ from appscale_tools import AppScaleTools
 from custom_exceptions import BadConfigurationException
 import parse_args
 
+
+class FakeIOString():
+  def __init__(self):
+    pass
+  def getvalue(self):
+    return "\n\n\n"
+
 class TestAppScaleDown(unittest.TestCase):
   def test_constructor(self):
     appscale_down = appscale_tools_thread.\
@@ -50,12 +57,144 @@ class TestAppScaleDown(unittest.TestCase):
     flexmock(appscale_thread).should_receive("get_completion_percentage").\
       and_return(percent).once()
     self.assertEquals({'status':'terminating', 'percent':percent}, appscale_thread.get_status())
-     
+   
   def test_completion_percentage(self):
     appscale_thread = appscale_tools_thread.AppScaleDown("cloud", "keyname")
-    flexmock(appscale_thread).should_receive("stdout_capture.getvalue.count").\
-      and_return(appscale_thread.EXPECTED_NUM_LINES).once()
-    self.assertEquals(94, appscale_thread.get_completition_percentage())
+    appscale_thread.std_out_capture = FakeIOString()
+    self.assertEquals(60, appscale_thread.get_completion_percentage())
+
+class TestAppScaleUp(unittest.TestCase):
+  def test_constructor(self):
+    appscale_up = appscale_tools_thread.\
+      AppScaleUp("cloud", "keyname", "a@a.com", "aaaaaa")
+    self.assertEquals(appscale_tools_thread.AppScaleUp.INIT_STATE,
+      appscale_up.state)
+
+  def test_run(self):
+    appscale_up = appscale_tools_thread.\
+      AppScaleUp("cloud", "keyname", "a@a.com", "aaaaaa")
+    flexmock(appscale_up).should_receive("appscale_up").and_return(True).\
+      once()
+    appscale_up.run()
+
+  def test_appscale_up(self):
+    appscale = appscale_tools_thread.\
+      AppScaleUp("cloud", "keyname", "a@a.com", "aaaaaa")
+    flexmock(appscale).should_receive("run_simple_cloud_deploy").and_return(True).\
+      once()
+    appscale.placement = appscale.SIMPLE
+    self.assertEquals(True, appscale.appscale_up())
+
+    appscale.placement = appscale.ADVANCE
+    flexmock(appscale).should_receive("run_simple_cloud_deploy").and_return(True).\
+      never()
+    flexmock(appscale).should_receive("run_advance_cloud_deploy").and_return(True).\
+      once()
+    self.assertEquals(True, appscale.appscale_up())
+ 
+    appscale.placement = "some_unknown_state" 
+    self.assertRaises(NotImplementedError, appscale.appscale_up)
+ 
+    appscale.deployment_type = appscale_tools_thread.CLUSTER
+    appscale.placement = "test dont care"
+    flexmock(appscale).should_receive("run_advance_cloud_deploy").and_return(True).\
+      never()
+    flexmock(appscale).should_receive("run_cluster_deploy").and_return(True).\
+      once()
+    self.assertEquals(True, appscale.appscale_up()) 
+
+    appscale.deployment_type = "some crazy deployment"
+    flexmock(appscale).should_receive("run_cluster_deploy").and_return(True).\
+      never()
+    self.assertRaises(NotImplementedError, appscale.appscale_up)
+
+  def test_run_addkeypair(self):
+    appscale = appscale_tools_thread.\
+      AppScaleUp("cloud", "keyname", "a@a.com", "aaaaaa")
+    class Args():
+      def __init__(self):
+        self.args = None
+    flexmock(parse_args).should_receive("ParseArgs").and_return(Args()).once()
+    flexmock(AppScaleTools).should_receive("add_keypair").once()
+    self.assertEquals(True, appscale.run_add_keypair())
+
+    flexmock(parse_args).should_receive("ParseArgs").and_return(Args()).once()
+    flexmock(AppScaleTools).should_receive("add_keypair").\
+      and_raise(BadConfigurationException).once()
+    self.assertEquals(False, appscale.run_add_keypair())
+
+    flexmock(parse_args).should_receive("ParseArgs").and_return(Args()).once()
+    flexmock(AppScaleTools).should_receive("add_keypair").\
+      and_raise(Exception).once()
+    self.assertEquals(False, appscale.run_add_keypair())
+  
+  def test_run_cluster_deploy(self):
+    appscale = appscale_tools_thread.\
+      AppScaleUp("cloud", "keyname", "a@a.com", "aaaaaa")
+    flexmock(appscale).should_receive("run_add_keypair").and_return(True).once()
+    flexmock(appscale).should_receive("run_appscale").and_return(True).once()
+    self.assertEquals(True, appscale.run_cluster_deploy())
+    
+    flexmock(appscale).should_receive("run_add_keypair").and_return(False).once()
+    flexmock(appscale).should_receive("run_appscale").and_return(True).never()
+    self.assertEquals(False, appscale.run_cluster_deploy())
+
+    flexmock(appscale).should_receive("run_add_keypair").and_return(True).once()
+    flexmock(appscale).should_receive("run_appscale").and_return(False).once()
+    self.assertEquals(False, appscale.run_cluster_deploy())
+
+  def test_run_advance_cloud_deploy(self):
+    appscale = appscale_tools_thread.\
+      AppScaleUp("cloud", "keyname", "a@a.com", "aaaaaa")
+    flexmock(appscale).should_receive("run_appscale").and_return(True).once()
+    self.assertEquals(True, appscale.run_advance_cloud_deploy())
+
+  def test_run_simple_cloud_deploy(self):
+    appscale = appscale_tools_thread.\
+      AppScaleUp("cloud", "keyname", "a@a.com", "aaaaaa")
+    flexmock(appscale).should_receive("run_appscale").and_return(True).once()
+    self.assertEquals(True, appscale.run_simple_cloud_deploy())
+
+  def test_run_appscale(self):
+    appscale = appscale_tools_thread.\
+      AppScaleUp("cloud", "keyname", "a@a.com", "aaaaaa")
+    class Args():
+      def __init__(self):
+        self.args = None
+    flexmock(appscale).should_receive("set_status_link").once()
+    flexmock(parse_args).should_receive("ParseArgs").and_return(Args())
+    flexmock(AppScaleTools).should_receive("run_instances").once()
+    self.assertEquals(True, appscale.run_appscale())
+
+    flexmock(AppScaleTools).should_receive("run_instances").and_raise(
+      BadConfigurationException).once()
+    flexmock(appscale).should_receive("set_status_link").never()
+    self.assertEquals(False, appscale.run_appscale())
+    
+    flexmock(AppScaleTools).should_receive("run_instances").and_raise(
+      Exception).once()
+    flexmock(appscale).should_receive("set_status_link").never()
+    self.assertEquals(False, appscale.run_appscale())
       
+    flexmock(AppScaleTools).should_receive("run_instances").and_raise(
+      SystemExit).once()
+    flexmock(appscale).should_receive("set_status_link").never()
+    self.assertEquals(False, appscale.run_appscale())
+
+  def test_get_status(self):
+    appscale = appscale_tools_thread.\
+      AppScaleUp("cloud", "keyname", "a@a.com", "aaaaaa")
+    self.assertEquals({'status': 'initializing', 'percent': 0}, appscale.get_status())
+
+    appscale.state = appscale.ERROR_STATE
+    self.assertEquals({'status': 'error', 'error_message' : "", 'percent': 0}, appscale.get_status())
+
+    appscale.state = appscale.RUNNING_STATE
+    flexmock(appscale).should_receive("get_completion_percentage").and_return(10)
+    self.assertEquals({'status': 'running', 'percent': 10}, appscale.get_status())
+
+    appscale.state = appscale.COMPLETE_STATE
+    self.assertEquals({'status': 'complete', 'link': None, 'percent': 100}, appscale.get_status())
+    
 if __name__ == "__main__":
   unittest.main()
