4c92a31ce58032d807ffa9db38d1e91b65f640e4
DrOctogon <spam4tyler@gmail.com>
2013-05-08 11:22:50 -0700

    input value changes slider position, rm unused js files

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -18,30 +18,23 @@ $(document).ready(function () {
 
 
 
-$(function() {
-    var options = {
-        min: 1,
-        max: 8,
-        values: [1],
-        slide: function(event, ui) {
-            $("#amount").val(ui.value);
-        }
-    }, min;
 
-    $("#slider-range").slider(options);
-    min = $("#slider-range").slider("values", 0);
 
-    $("#amount").val("" + min );
 
-});
 
-$(document).ready(); {
-    $("#slider_input").change(function(){
-
-         var index = $(this).attr('class').substring(0,1);
-        $("#slider_"+ index).slider('value', this.value);
-        refreshSliders( index - 0 );
+$("#slider").slider({
+    range: "min",
+    value: 1,
+    min: 0,
+    max: 8,
+    slide: function( event, ui ) {
+        $( "#amount" ).val( ui.value );
+    }
 });
 
 
-
+$("#amount").change(function () {
+    var value = this.value.substring();
+    console.log(value);
+    $("#slider").slider("value", parseInt(value));
+});
