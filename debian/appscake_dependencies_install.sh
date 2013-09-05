da52511548a20a4cd5016bfa6a327f258723ab11
Jovan Chohan <jovan@appscale.com>
2013-09-05 11:09:42 -0700

    making build script more robust

--- debian/appscake_dependencies_install.sh
+++ debian/appscake_dependencies_install.sh
@@ -1,24 +1,26 @@
-wget https://s3.amazonaws.com/appscale-build/setuptools-0.6c11.tar.gz
-tar zxvf setuptools-0.6c11.tar.gz
-cd setuptools-0.6c11
-python setup.py install
-
-cd ..
-rm -r setuptools-0.6c11
-rm setuptools-0.6c11.tar.gz
+echo "Installing setuptools if needed."
+set +e
+hash easy_install > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+  set -e
+  echo "setuptools not found - installing."
+  mkdir -pv downloads
+  cd downloads
+  wget https://s3.amazonaws.com/appscale-build/setuptools-0.6c11.tar.gz
+  tar zxvf setuptools-0.6c11.tar.gz
+  pushd setuptools-0.6c11
+  python setup.py install
+  popd
+  rm -fr setuptools-0.6c11*
+fi
+set -e
 
 easy_install termcolor
-easy_install M2Crypto 
 easy_install SOAPpy
 easy_install pyyaml
 easy_install boto==2.6
 easy_install argparse
 easy_install django 
+easy_install google-api-python-client
 apt-get -y install expect
 
-apt-get -y install mercurial
-hg clone https://code.google.com/p/google-api-python-client/
-cd google-api-python-client
-python setup.py install
-cd ..
-rm -r google-api-python-client
