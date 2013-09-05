ca3af4f9e207a3f778dd01a30f6ac557bfd3f1a4
Jovan Chohan <jovan@appscale.com>
2013-09-05 09:49:32 -0700

    fixing build script order

--- debian/appscake_dependencies_install.sh
+++ debian/appscake_dependencies_install.sh
@@ -1,3 +1,12 @@
+wget https://s3.amazonaws.com/appscale-build/setuptools-0.6c11.tar.gz
+tar zxvf setuptools-0.6c11.tar.gz
+cd setuptools-0.6c11
+python setup.py install
+
+cd ..
+rm -r setuptools-0.6c11
+rm setuptools-0.6c11.tar.gz
+
 easy_install termcolor
 easy_install M2Crypto 
 easy_install SOAPpy
@@ -6,15 +15,6 @@ easy_install boto==2.6
 easy_install argparse
 easy_install django 
 apt-get -y install expect
-
-wget https://s3.amazonaws.com/appscale-build/setuptools-0.6c11.tar.gz
-tar zxvf setuptools-0.6c11.tar.gz
-cd setuptools-0.6c11
-python setup.py install
-
-cd ..
-rm -r setuptools-0.6c11  
-rm setuptools-0.6c11.tar.gz
 
 apt-get -y install mercurial
 hg clone https://code.google.com/p/google-api-python-client/
